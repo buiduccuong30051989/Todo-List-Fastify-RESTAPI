998c069a6b857b35ff9e3abba7be3a1d51fe511f
cuong.duc.bui@advesa.com <cuong.duc.bui@advesa.com>
2024-06-06 14:43:23 +0700

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,128 @@
-import * as fastify from 'fastify';
-import mongoose from 'mongoose';
-import routes from './routes';
-import { Options } from './config/swagger';
-import { config } from './config/app';
+import * as fastify from "fastify";
+import mongoose from "mongoose";
+import routes from "./routes";
+import { Options } from "./config/swagger";
+import { config } from "./config/app";
 const env = process.env.NODE_ENV;
-import fastifySwagger from '@fastify/swagger';
-import fastifySwaggerUI from '@fastify/swagger-ui';
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+import { ConnectOptions } from "mongoose";
+
+// const uri =
+//     "mongodb+srv://frontendcuong30051989:002Dxpdd2qU2FrRR@cluster0.xkmavbs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://frontendcuong30051989:002Dxpdd2qU2FrRR@cluster0.xkmavbs.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=Cluster0";
+// const client = new MongoClient(uri);
+
+// async function connectToDatabase() {
+//   try {
+//     await client.connect();
+//     const db = client.db('todo-app');
+//     tasksCollection = db.collection('tasks');
+//     console.log('Connected to the database');
+//   } catch (error) {
+//     console.error('Error connecting to the database:', error);
+//   }
+// }
+
+// async function run() {
+//     try {
+//         // Connect the client to the server	(optional starting in v4.7)
+//         await client.connect();
+
+//         // Send a ping to confirm a successful connection
+// await client.db("admin").command({ ping: 1 });
+// const users = await client
+//     .db("sample_mflix")
+//     .collection("users")
+//     .find()
+//     .toArray();
+//         console.log("List of users:");
+//         console.log(users);
+//         console.log(
+//             "Pinged your deployment. You successfully connected to MongoDB!",
+//         );
+//     } finally {
+//         // Ensures that the client will close when you finish/error
+//         await client.close();
+//     }
+// }
+
+// run();
 
 // Configure App
 const app = fastify.default({ logger: true });
 
 app.register(fastifySwagger, Options);
 app.register(fastifySwaggerUI, {
-	routePrefix: '/docs',
-	uiConfig: {
-	  docExpansion: 'full',
-	  deepLinking: false
-	}
+    routePrefix: "/docs",
+    uiConfig: {
+        docExpansion: "full",
+        deepLinking: false,
+    },
 });
 
-routes.forEach(route => {
-	app.register((app, options, done) => {
-		app.route(route);
-		done();
-	});
+routes.forEach((route) => {
+    app.register((app, options, done) => {
+        app.route(route);
+        done();
+    });
 });
+
+// Define a schema
+const UserSchema = new mongoose.Schema(
+    {},
+    { collection: "users", strict: false },
+);
+
+// Define a model
+const User = mongoose.model("User", UserSchema, "users");
+
+mongoose
+    .connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    } as ConnectOptions)
+    .then(async () => {
+        app.log.info("MongoDB connected...");
+
+        // Query data
+        const users = await User.find().exec();
+        console.log("List of users:");
+        console.log(users);
+    })
+    .catch((err) => console.log(err));
 
 const start = async (): Promise<void> => {
-	try {
-		await app.ready();
-		app.swagger();
-		await app.listen({ port: config.app.port });
-	} catch (err) {
-		app.log.error(err);
-		process.exit(1);
-	}
+    try {
+        await app.ready();
+        app.swagger();
+        await app.listen({ port: config.app.port });
+    } catch (err) {
+        app.log.error(err);
+        process.exit(1);
+    }
 };
 start();
 
 export default app;
 
 // Configure DB
-if (env !== 'test') {
-	mongoose
-		.connect(`mongodb://${config.db.host}:${config.db.port}/${config.db.name}`, { })
-		.then(() => app.log.info('MongoDB connected...'))
-		.catch(err => app.log.error(err));
-}
+// if (env !== "test") {
+//     mongoose
+//         .connect(
+//             `mongodb://${config.db.host}:${config.db.port}/${config.db.name}`,
+//             {},
+//         )
+//         .then(() => app.log.info("MongoDB connected..."))
+//         .catch((err) => app.log.error(err));
+// }
+
+mongoose
+    .connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    } as ConnectOptions)
+    .then(() => app.log.info("MongoDB connected..."))
+    .catch((err) => app.log.error(err));
